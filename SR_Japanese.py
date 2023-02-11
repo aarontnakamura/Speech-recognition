72f2d388c5426681af7bd1177fcb4ecd012d7023
Aaron Nakamura <63680964+aarontnakamura@users.noreply.github.com>
2023-02-11 18:43:45 +0900

    Update SR_Japanese.py

--- SR_Japanese.py
+++ SR_Japanese.py
@@ -2,8 +2,8 @@
 import speech_recognition as sr
 
 r = sr.Recognizer()
-
-with sr.AudioFile('/Users/aarontnakamura/Desktop/export/4_resting_state_long_1_2022_Sep_20_1925_wav/mic_alt-1663669655.877.wav') as source:
+#replace with the file name you want to use
+with sr.AudioFile('abcdefg.wav') as source:
     audio = r.record(source)
 print(r.recognize_google(audio, language='ja-JP'))
 
